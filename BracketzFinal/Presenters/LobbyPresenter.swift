91d9e70b4497ac26a265f96645aacf94f4c9c94f
Edward McGuiggan <edwardmcguiggan@mac.mynetworksettings.com>
2026-09-11 01:08:04 -0400

    Stabilize lobby player ordering and pairing

--- BracketzFinal/Presenters/LobbyPresenter.swift
+++ BracketzFinal/Presenters/LobbyPresenter.swift
@@ -15,7 +15,9 @@ class LobbyPresenter {
     let tournyBuyIn = 1
     var tournyUserIDs = [String]()
     var count = 1
-    var tourny: Tournament?
+    var tourny: Tournament
+    var matchesArray = [String]()
+    var users = [User]()
     
     init(_ view: LobbyVC, currentUser: User, tournySize: Int, tourny: Tournament) {
         self.view = view
@@ -26,41 +28,29 @@ class LobbyPresenter {
     }
     
     func fetchUsers() {
-        var array = [User]()
-        for x in tourny!.tournamentUsers {
-            Service.shared.fetchUserData(uid: x) { (user) in
-                array.append(user)
-                self.users = array
-            }
-        }
-    }
-    
-    
-    var matchesArray: [String]? {
-        didSet {
-            if matchesArray?.count == tournySize/2 {
-                setMatch()
-            }
-        }
-    }
-    
-    var users: [User]? {
-        didSet {
-            if users?.count == tournySize {
-                if count == 1{
-                    view.reloadData()
-                    view.configureUI()
-                    observeTournament()
-                }
+        var usersByID = [String: User]()
+        let userIDs = tourny.tournamentUsers
+
+        for userID in userIDs {
+            Service.shared.fetchUserData(uid: userID) { user in
+                usersByID[userID] = user
+
+                guard usersByID.count == userIDs.count else { return }
+                self.users = userIDs.compactMap { usersByID[$0] }
+
+                guard self.users.count == self.tournySize, self.count == 1 else { return }
+                self.view.reloadData()
+                self.view.configureUI()
+                self.observeTournament()
             }
         }
     }
     
     func observeTournament() {
-        Service.shared.observePresentUsers(uid: tourny!.tournamentID) { (presentUsers) in
+        Service.shared.observePresentUsers(uid: tourny.tournamentID) { (presentUsers) in
             self.view.waitingOnLabel.text = "waiting on \(self.tournySize - presentUsers) users"
             if presentUsers == self.tournySize {
-                self.tourny!.acceptedUsers = self.tournySize
+                self.tourny.acceptedUsers = self.tournySize
                 self.view.dimBackground()
                 self.checkIfCurrentUserIsHost()
             }
@@ -68,33 +58,27 @@ class LobbyPresenter {
     }
     
     func checkIfCurrentUserIsHost() {
-        if currentUser.uid == self.users![0].uid {
+        guard let host = users.first else { return }
+
+        if currentUser.uid == host.uid {
             configureTournament()
         } else {
-            self.count += 1
+            count += 1
+            tournyUserIDs = users.map(\.uid)
             observeMatches()
-            for x in 0..<self.users!.count {
-                self.tournyUserIDs.append(self.users![x].uid)
-            }
         }
     }
     
     func configureTournament() {
         
-            if count == 1 {
-                for x in 0..<users!.count {
+            if count == 1, users.count == tournySize {
+                for x in 0..<users.count {
                     if x%2 == 0 {
-                        var matchUsers = [User]()
-                        var usernames = [String]()
-                        matchUsers.append(users![x])
-                        matchUsers.append(users![x+1])
-                        usernames.append(users![x].uid)
-                        usernames.append(users![x+1].uid)
-                        tournyUserIDs.append(users![x].uid)
-                        tournyUserIDs.append(users![x+1].uid)
+                        let usernames = [users[x].uid, users[x + 1].uid]
+                        tournyUserIDs.append(contentsOf: usernames)
                         
                         let values = ["users": usernames] as [String: Any]
-                        REF_TOURNAMENTS.child(tourny!.tournamentID).child("matches").childByAutoId().updateChildValues(values)
+                        REF_TOURNAMENTS.child(tourny.tournamentID).child("matches").childByAutoId().updateChildValues(values)
                     }
                 }
                 observeMatches()
@@ -104,33 +88,26 @@ class LobbyPresenter {
     }
     
     func observeMatches() {
-        Service.shared.observeMatches(uid: tourny!.tournamentID) { (matches) in
+        Service.shared.observeMatches(uid: tourny.tournamentID) { (matches) in
             if matches.count == self.tournySize/2 {
                 self.matchesArray = matches
+                self.setMatch()
             }
         }
     }
     
     func setMatch() {
-        let sortedMatches = matchesArray!.sorted()
-        var matchIndex = Int()
-        var myMatchId = String()
-        var finalUsers = [User]()
-        
-        for x in 0..<tournyUserIDs.count {
-            if x%2 == 0 {
-                var matchUsers = [User]()
-                matchUsers.append(users![x])
-                matchUsers.append(users![x+1])
-                
-                if tournyUserIDs[x] == currentUser.uid || tournyUserIDs[x+1] == currentUser.uid {
-                    matchIndex = x/2
-                    finalUsers = matchUsers
-                    myMatchId = sortedMatches[matchIndex]
-                }
-            }
-            
-        }
+        let sortedMatches = matchesArray.sorted()
+        guard users.count == tournyUserIDs.count,
+              sortedMatches.count == tournySize / 2,
+              let playerIndex = tournyUserIDs.firstIndex(of: currentUser.uid) else { return }
+
+        let pairStartIndex = playerIndex - (playerIndex % 2)
+        guard users.indices.contains(pairStartIndex + 1),
+              sortedMatches.indices.contains(pairStartIndex / 2) else { return }
+
+        let finalUsers = [users[pairStartIndex], users[pairStartIndex + 1]]
+        let myMatchId = sortedMatches[pairStartIndex / 2]
         
         view.waitingOnLabel.textColor = .green
         view.waitingOnLabel.text = "Pot: $\(tournyBuyIn)"
@@ -139,7 +116,7 @@ class LobbyPresenter {
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
                 self.view.shouldPresentLoadingView(false)
-                let controller = MatchPlayVC(finalUsers, self.tourny!, myMatchId, self.tournySize, self.currentUser)
+                let controller = MatchPlayVC(finalUsers, self.tourny, myMatchId, self.tournySize, self.currentUser)
                 self.view.navigationController?.popToRootViewController(animated: true)
                 self.view.navigationController?.pushViewController(controller, animated: true)
             }
